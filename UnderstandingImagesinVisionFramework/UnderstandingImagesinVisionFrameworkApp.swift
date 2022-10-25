8098488a87ab2a7d6c6132451ac59c723364f358
홍성민 <seanhong2000@gmail.com>
2022-10-26 03:18:41 +0900

    Initial Commit

---
+++ UnderstandingImagesinVisionFramework/UnderstandingImagesinVisionFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnderstandingImagesinVisionFrameworkApp.swift
+//  UnderstandingImagesinVisionFramework
+//
+//  Created by Sean Hong on 2022/10/26.
+//
+
+import SwiftUI
+
+@main
+struct UnderstandingImagesinVisionFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
